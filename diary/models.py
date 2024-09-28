dcb7b7c5b8476b7e7e38d6636b8873aaaf7dc906
Aliya Suleymanova <aliyasul2579@gmail.com>
2024-09-28 19:50:41 +0500

    Кеширование

--- diary/models.py
+++ diary/models.py
@@ -1,16 +1,15 @@
 from django.db import models
 from users.models import User
-from django.utils import timezone
+
 
 NULLABLE = {'blank': True, 'null': True}
 
 
 class DiaryEntry(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Наименование")
-    description = models.TextField(verbose_name='Описание', **NULLABLE)
+    name = models.CharField(max_length=100, verbose_name="Заголовок")
+    description = models.TextField(verbose_name='Описание события', **NULLABLE)
     image = models.ImageField(upload_to='diary/', verbose_name='Изображение', **NULLABLE)
-    date_of_event = models.DateField(verbose_name='Дата события', help_text='в формате 2024-01-31',
-                                     default='2024-01-01')
+    date_of_event = models.DateField(**NULLABLE, verbose_name='Дата события', help_text='в формате ГГ-ММ-ДД')
     created_at = models.DateField(auto_now_add=True, **NULLABLE, verbose_name='Дата создания (записи в БД)')
     updated_ad = models.DateField(auto_now_add=True, **NULLABLE, verbose_name='Дата последнего изменения (записи в БД)')
     owner = models.ForeignKey(User, on_delete=models.SET_NULL, verbose_name='Пользователь', **NULLABLE)
